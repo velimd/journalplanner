4f5f0e7574b5ee2d3306109529bfcb2f89c6dd18
velimd <velican.duzgunce@solirius.com>
2018-08-29 15:51:49 +0100

    included delete confirmation

--- frontend/client/src/pages/EditResource.js
+++ frontend/client/src/pages/EditResource.js
@@ -267,7 +267,29 @@ class EditResource extends Component {
                                 <button className="btn btn-primary" type="submit" id="btn-add-submit">submit</button>
                             </form>
                             <div>
-                                <button type="button" className="btn btn-danger" onClick={this.delete.bind(this)}>Delete</button>
+                                <button type="button" className="btn btn-danger" data-toggle="modal" data-target="#deleteConfirm">Delete</button>
+
+                                <div className="modal fade" id="deleteConfirm" tabIndex="-1" role="dialog"
+                                     aria-labelledby="deleteConfirmLabel" aria-hidden="true">
+                                    <div className="modal-dialog" role="document">
+                                        <div className="modal-content">
+                                            <div className="modal-header">
+                                                <h5 className="modal-title" id="deleteConfirmLabel">Delete Confirmation</h5>
+                                                <button type="button" className="close" data-dismiss="modal"
+                                                        aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div className="modal-body">
+                                                Are you sure you want to delete this item?
+                                            </div>
+                                            <div className="modal-footer">
+                                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                <button type="button" className="btn btn-danger" onClick={this.delete.bind(this)} data-dismiss="modal">Delete</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
